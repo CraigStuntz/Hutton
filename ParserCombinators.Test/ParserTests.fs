676fb81c9376616920e67159a34e35e3d7823092
Craig Stuntz <craigstuntz@gmail.com>
2015-11-23 09:31:11 -0500

    Initial commit

---
+++ ParserCombinators.Test/ParserTests.fs
@@ -0,0 +1,161 @@
+﻿namespace TinyLangage.Tests.Lexer
+
+open NUnit.Framework
+open FsUnit
+open Parsers
+
+type ParserTests () =
+    [<Test>]
+    member this.``return 1 works``() =
+        let actual = parse (parser { return 1 }) "abc"
+        actual |> should equal [(1, "abc")]
+
+    [<Test>]
+    member this.``failure returns empty list``() =
+        let actual = parse failure "abc"
+        actual |> should equal []
+
+    [<Test>]
+    member this.``item with empty string returns empty list``() =
+        let actual = parse item ""
+        actual |> should equal []
+    
+    [<Test>]
+    member this.``item "abc" returns 'a'``() =
+        let actual = parse item "abc"
+        actual |> should equal [('a', "bc")]
+
+    [<Test>]
+    member this.``computation expressions with Bind and Return work``() =
+        let p : Parser<char * char> = 
+            parser {
+                let! x = item
+                let! _ = item
+                let! y = item
+                return (x, y)
+            }
+        let actual = parse p "abcdef" 
+        actual |> should equal [(('a', 'c'), "def")]
+
+    [<Test>]
+    member this.``computation expressions with Bind and Return work on bad match``() =
+        let p : Parser<char * char> = 
+            parser {
+                let! x = item
+                let! _ = item
+                let! y = item
+                return (x, y)
+            }
+        let actual = parse p "ab" 
+        actual |> should equal []
+
+    [<Test>]
+    member this.``choice with p``() = 
+        let actual = parse (item +++ (parser { return 'd'})) "abc"
+        actual |> should equal [('a', "bc")]
+
+    [<Test>]
+    member this.``choice with q``() = 
+        let actual = parse (failure +++ (parser { return 'd'})) "abc"
+        actual |> should equal [('d', "abc")]
+
+    [<Test>]
+    member this.``choice with failure``() = 
+        let actual = parse (failure +++ failure) "abc"
+        actual |> should equal []
+
+    [<Test>]
+    member this.``digit parses "1"``() =
+        let actual = parse digit "123"
+        actual |> should equal [('1', "23")]
+    
+    [<Test>]
+    member this.``digit fails on "abc"``() =    
+        let actual = parse digit "abc"
+        actual |> should equal []
+        
+    [<Test>]
+    member this.``char parses "a"``() =
+        let actual = parse (charParser 'a') "abc"
+        actual |> should equal [('a', "bc")]
+    
+    [<Test>]
+    member this.``char fails on "123"``() =
+        let actual = parse (charParser 'a') "123"
+        actual |> should equal []
+    
+    [<Test>]
+    member this.``stringParser parses "abc"``() =
+        let actual = parse (stringParser "abc") "abcdef"
+        actual |> should equal [("abc", "def")]
+    
+    [<Test>]
+    member this.``many digit parses "123abc"``() =
+        let actual = parse (many digit) "123abc"
+        actual |> should equal [(['1'; '2'; '3'], "abc")]
+
+    [<Test>]
+    member this.``many digit parses "abcdef"``() =
+        let actual = parse (many digit) "abcdef"
+        // F# type inference gets the type of [] wrong and "should equal" fails 
+        // if we don't give it explicit help!
+        // This is mostly a bug in NUnit/FsUnit: https://github.com/fsprojects/FsUnit/issues/78
+        let expectedList : char list = []
+        let expected = [(expectedList, "abcdef")]
+        actual |> should equal expected
+
+    [<Test>]
+    member this.``many1 digit fails on "abcdef"``() =
+        let actual = parse (many1 digit) "abcdef"
+        actual |> should equal []
+
+    [<Test>]
+    member this.``ident works``() =
+        let actual = parse ident "abc def"
+        actual |> should equal [("abc", " def")]
+
+    [<Test>]
+    member this.``nat works``() =
+        let actual = parse nat "123 abc"
+        actual |> should equal [(123, " abc")]
+
+    [<Test>]
+    member this.``space works``() =
+        let actual = parse space "    abc"
+        actual |> should equal [((), "abc")]
+
+    [<Test>]
+    member this.``can parse non-empty list of natural numbers and ignore spacing``() = 
+        let p : Parser<int list> = 
+            let natAndComma = parser {
+                let! _ = symbol ","
+                return! natural
+            }
+            parser {
+                let! _ = symbol "["
+                let! n = natural
+                let! ns = many natAndComma
+                let! _ = symbol "]"
+                return (n :: ns)
+            }
+        parse p " [1, 2, 3] " |> should equal [([1; 2; 3], "")]
+        parse p "[1, 2, ]"    |> should equal []
+
+
+    [<Test>]
+    member this.``can eval 2*3+4``() = 
+        let actual = eval "2*3+4"
+        actual |> should equal 10
+
+    [<Test>]
+    member this.``can eval 2*(3+4)``() = 
+        let actual = eval "2*(3+4)"
+        actual |> should equal 14
+
+    [<Test>]
+    member this.``eval fails on 2*3-4``() = 
+        (fun () -> eval "2*3-4" |> ignore) |> should (throwWithMessage "Unused input -4") typeof<System.Exception>
+
+    [<Test>]
+    member this.``eval fails on -1``() = 
+        (fun () -> eval "-1" |> ignore) |> should (throwWithMessage "Invalid input -1") typeof<System.Exception>
---
+++ ParserCombinators/Parser.fs
@@ -0,0 +1,185 @@
+﻿module Parsers
+
+open System
+
+
+type Parser<'a>      = (char list) -> ('a * (char list)) list
+
+
+type ParserBuilder() =
+
+    member this.Bind(p, f)    = p >> List.collect (fun (a, inp) -> (f a) inp)
+
+    member this.Return(x)     = fun (inp: (char list)) -> [(x, inp)]
+
+    member this.ReturnFrom(m) = m
+
+    member this.Zero()        = []
+
+
+let parser = new ParserBuilder()
+
+
+let failure : Parser<'a> =
+    fun _ -> []
+
+
+/// Fails if input char list is empty.
+/// Succeeds with first char otherwise
+let item : Parser<char> = function
+| []      -> []
+| x :: xs -> [(x, xs)]
+
+
+let parse (p : Parser<'a>) (inp : string) : ('a * string) list = 
+    p (inp |> List.ofSeq) 
+        |> List.map (fun (a, chars) -> (a, chars |> String.Concat))
+
+
+let (+++) (p: Parser<'a>) (q: Parser<'a>) : Parser<'a> = 
+    fun (inp: char list) ->
+        match inp |> p with
+        | []      -> inp |> q
+        | success -> success
+
+
+let sat (p: char -> bool) : Parser<char> =
+    parser {
+        let! x = item
+        if (p x)
+        then return x
+        else return! failure
+    }
+
+
+let digit: Parser<char> = 
+    sat Char.IsDigit
+
+
+let lower: Parser<char> =
+    sat Char.IsLower
+
+
+let upper: Parser<char> =
+    sat Char.IsUpper
+
+
+let letter: Parser<char> = 
+    sat Char.IsLetter
+
+
+let alphanum: Parser<char> =
+    sat Char.IsLetterOrDigit
+
+
+let charParser (x: char) : Parser<char> =
+    sat ((=) x)
+
+
+let rec stringParser (str: string) : Parser<string> =
+    parser {
+        match str with
+        | "" -> return ""
+        | _  -> 
+            let! _ = charParser str.[0]
+            let! _ = stringParser (str.Substring 1)
+            return str
+    }
+
+
+let rec many (p: Parser<'a>) : Parser<'a list> =
+    many1 p +++ parser { return [] }
+and many1 (p: Parser<'a>) : Parser<'a list> =
+    parser {
+        let! v = p
+        let! vs = many p
+        return (v :: vs)
+    }
+
+
+let ident : Parser<string> =
+    parser {
+        let!   x  = lower
+        let!   xs = many alphanum
+        return 
+            x :: xs 
+            |> String.Concat
+    }
+
+
+let nat : Parser<int> =
+    parser {
+        let!   xs = many1 digit
+        return 
+            xs 
+            |> String.Concat 
+            |> Int32.Parse
+    }
+
+
+let space : Parser<unit> =
+    parser {
+        let! _ = many (sat Char.IsWhiteSpace)
+        return ()
+    }
+
+
+let token (p: Parser<'a>) : Parser<'a> =
+    parser {
+        let! _ = space
+        let! v = p
+        let! _ = space
+        return v
+    }
+
+
+let identifier : Parser<string> =
+    token ident
+
+
+let natural : Parser<int> = 
+    token nat
+
+
+let symbol (xs: string) : Parser<string> =
+    token <| stringParser xs
+
+
+
+
+let rec expr : Parser<int> =
+    plusExpr +++ term
+and plusExpr : Parser<int> =
+    parser {
+        let! t = term
+        let! _ = symbol "+"
+        let! e = expr
+        return (t + e) 
+    }
+and term : Parser<int> = 
+    timesExpr +++ factor
+and timesExpr : Parser<int> =
+        parser {
+            let! f = factor
+            let! _ = symbol "*"
+            let! t = term
+            return (f * t)
+        }
+and factor : Parser<int> = 
+    parenthetical +++ natural
+and parenthetical : Parser<int> = 
+    parser {
+        let! _ = symbol "("
+        let! e = expr
+        let! _ = symbol ")"
+        return e
+    }
+
+
+
+
+let eval (xs: string) : int =
+    match parse expr xs with
+    | [(n, "")]  -> n
+    | [(_, out)] -> failwithf "Unused input %s" out
+    | _          -> failwithf "Invalid input %s" xs
